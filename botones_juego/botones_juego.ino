9597fa5f3e30b2c4b26264558a3f5b5562c3e63e
Jose Lara <jolapa20@gmail.com>
2022-07-25 09:46:03 -0500

    Added folder for ino file

--- botones_juego/botones_juego.ino
+++ botones_juego/botones_juego.ino
@@ -2,38 +2,41 @@
 #define btn2 5
 #define btn3 6
 #define btn4 9
+#define btn5 8
 void setup() {
   Serial.begin(9600);
   pinMode(btn1, INPUT);
   pinMode(btn2, INPUT);
   pinMode(btn3, INPUT);
   pinMode(btn4, INPUT);
+  pinMode(btn5, INPUT);
   Serial.println(0);
 }
 
 void loop() {
   if (digitalRead(btn1) == HIGH)
   {
-    Serial.println(1);
-    delay(100);
+    Serial.println(1); //UP
   }
       else if (digitalRead(btn2) == HIGH)
   {
-    Serial.println(2);
-    delay(100);
+    Serial.println(2);//DOWN
   }
      else if (digitalRead(btn3) == HIGH)
   {
-    Serial.println(3);
-    delay(100);
+    Serial.println(3);//LEFT
   }
      else if (digitalRead(btn4) == HIGH)
   {
-    Serial.println(4);
-    delay(100);
+    Serial.println(4);//RIGHT
+  }
+     else if (digitalRead(btn5) == HIGH)
+  {
+    Serial.println(5);//SHOOT
   }
   else{
-    Serial.println(0);
-    delay(150);
+    Serial.println(0);//Normal state
   }
+  delay(100);
+  
 }
